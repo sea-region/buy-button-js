48f0b3554173397eff960f856ab4221b5d7b0e92
Tessa Thornton <tessathornton@gmail.com>
2016-06-20 14:30:05 -0400

    yoyo?

--- src/main.js
+++ src/main.js
@@ -1,118 +1,95 @@
-import "babel-polyfill";
-import productTemplate from './templates/product';
-import Collection from './components/collection';
-import Product from './components/product';
-import Cart from './components/cart';
-import ShopifyBuy from 'shopify-buy';
+import yo from 'yo-yo';
 
-window.ShopifyBuy = ShopifyBuy;
+class Product {
+  constructor(props) {
+    this.data = {
+      title: "horse mask",
+      id: 1,
+    }
+    this.props = props;
+    this.dom = this.render();
+    document.body.appendChild(this.dom)
+  }
 
-const componentTypes = {
-  'product': Product,
-  'collection': Collection
+  onclick(e) {
+    this.props.onclick(this.data);
+  }
+
+  templates(data) {
+    return {
+      title: `<h2>${data.title}</h2>`,
+      button: function (onclick) {
+        return yo`<button onclick=${onclick.bind(this)}>Buy</button>`
+      }
+    }
+  }
+
+  render() {
+    let defaultTemplates = this.templates(this.data);
+    let templates = Object.assign({}, defaultTemplates, this.props.templates);
+    return yo`
+      <div>
+        ${templates.title(this.data)}
+        ${templates.button(this.onclick.bind(this))}
+      </div>
+    `
+  }
+}
+
+class Cart {
+  constructor(props) {
+    this.data = {
+      lineItems: []
+    }
+    this.props = props;
+    this.dom = this.render();
+    document.body.appendChild(this.dom)
+  }
+
+  addItem(item) {
+    this.data.lineItems.push(item);
+    let newDom = this.render();
+    yo.update(this.dom, newDom);
+  }
+
+  render() {
+    return yo`
+      <div>
+        <h3>Your cart</h3>
+        <ul>
+          ${this.data.lineItems.map(item => {
+            return yo`<li>${item.title}</li>`
+          })}
+        </ul>
+      </div>
+    `
+  }
 }
 
 class UI {
   constructor() {
-    this.components = {
-      'collection': [],
-      'product': []
-    };
-    this.client = ShopifyBuy.buildClient({
-      apiKey: 'bf081e860bc9dc1ce0654fdfbc20892d',
-      myShopifyDomain: 'embeds',
-      appId: '6'
-    });
-    this.loadEmbedStyles(() => {
-      this.cart = new Cart({}, {
-        client: this.client
-      });
-      this.onReady();
-    });
+    this.cart = new Cart();
   }
 
-  addVariantToCart(data) {
-    this.cart.addItem(data);
+  createProduct(props) {
+    let product = new Product(Object.assign({}, props, {onclick: this.addToCart.bind(this)}));
   }
 
-  get props() {
-    return {
-      collection: {
-        'addVariantToCart': this.addVariantToCart.bind(this)
-      },
-      product: {
-        'addVariantToCart': this.addVariantToCart.bind(this)
-      }
-    }
-  }
-
-  loadEmbedStyles(cb) {
-    let cssURL = './styles/embeds.css';
-
-    let link = document.createElement('link');
-
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = cssURL;
-
-    let img = document.createElement('img');
-
-    img.style.opacity = 0;
-    document.body.appendChild(img);
-    document.head.appendChild(link);
-
-
-    img.src = cssURL;
-    img.onerror = () => {
-      document.body.removeChild(img);
-      cb();
-    }
-
-}
-
-  createComponent(type, config) {
-    let props = {
-      callbacks: this.props[type],
-      client: this.client
-    }
-    this.components[type].push(new componentTypes[type](config, props));
+  addToCart(item) {
+    this.cart.addItem(item);
   }
 }
 
-ShopifyBuy.UI = new UI();
+function pass() {
+  return arguments;
+}
 
-ShopifyBuy.UI.onReady = () => {
-  // ShopifyBuy.UI.createComponent('product', {
-  //   id: 6640244678,
-  //   styles: {
-  //     button: {
-  //       'background-color': 'red',
-  //       'color': 'black'
-  //     }
-  //   }
-  // });
+const ui = new UI();
 
-ShopifyBuy.UI.createComponent('collection', {
-    id: 244484358,
-    productConfig: {
-      contents: ['img', 'title', 'variantTitle', 'price', 'description', 'variantSelection', 'button'],
-      templates: {
-        title: '<h4>{{data.title}}</h4>',
-        description: '<p>{{{data.attrs.body_html}}}</p>',
-        button: '<button data-event="click.addVariantToCart" class="my-fancy-button-class {{classes.button}}">Special Add To Cart</button>'
-      },
-      styles: {
-        button: {
-          'background-color': 'red',
-          'color': 'yellow',
-          'border': '0',
-          'border-radius': '5px',
-          'font-size': '16px'
-        }
-      },
-      addVariantToCart: (product) => {
-        console.log(`product ${product.id} added to cart`);
-      }
-    }
-  });
-};
+ui.createProduct({
+  templates: {
+    title: function (data) {
+       return yo`<h1><small>Featured Product:</small> ${data.title}</h1>`
+     }
+  }
+});
